49dc07f1f14c92242cdf73a3fed40779b5caa519
maojingke <mjk0120@163.com>
2020-10-25 13:54:10 +0800

    猜数字程序

--- caishuzi.py
+++ caishuzi.py
@@ -1,17 +1,20 @@
 import random as r
 a= r.randint(0,100)#随机产生一个0到100的整数
 for i in range(10):
-             b=int(input("请输入一个0到100的正整数："))#用户获取一个输入
-             if b>=0 and b<=100:
-                          if b<a:
-                                       print("猜小了，共10次机会，还剩{}次".format(9-i))
-                          elif b>a:
-                                       print("猜大了，共10次机会，还剩{}次".format(9-i))
-                          elif b==a:
-                                       print("恭喜您，猜对了，该数字为{}".format(a))
-                                       break
+             b=input("请输入一个0到100的正整数：")#用户获取一个输入
+             if b.isnumeric():
+                          if int(b)>=0 and int(b)<=100:
+                                       if int(b)<a:
+                                                    print("猜小了，共10次机会，还剩{}次".format(9-i))
+                                       elif int(b)>a:
+                                                    print("猜大了，共10次机会，还剩{}次".format(9-i))
+                                       elif int(b)==a:
+                                                    print("恭喜您，猜对了，该数字为{}".format(a))
+                                                    break
+                          else:
+                                       print("您并没有输入0到100的整数，浪费了一次机会")
              else:
-                          print("您并没有输入0到100的整数，浪费了一次机会")
+                          print("您没有输入数字，请输入0到100的整数")
              while i == 9:
                           print("机会已用尽，您并没有猜出数字，游戏结束")
                           break
